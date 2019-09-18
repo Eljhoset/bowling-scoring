297c8ef1fe84e630069c853fd21474e6574b7098
jd-jd <jd-jd@DESKTOP-MOFF8P0>
2019-09-18 13:09:46 -0400

    complete formatter

--- src/test/java/com/eljhoset/bowlingscoring/formatter/DefaultPlayerScoreFormatterIT.java
+++ src/test/java/com/eljhoset/bowlingscoring/formatter/DefaultPlayerScoreFormatterIT.java
@@ -33,61 +33,65 @@ public class DefaultPlayerScoreFormatterIT {
     @Before
     public void setup() throws Exception {
 
-        final String filePathRegularGame = "src/test/resources/regular_game.txt";
-        final String filePathRegularGameTwoPlayers = "src/test/resources/regular_game_two_players.txt";
-        final String filePathAllStrike = "src/test/resources/all_strikes.txt";
-        final String filePathAllFouls = "src/test/resources/all_fouls.txt";
+	final String filePathRegularGame = "src/test/resources/regular_game.txt";
+	final String filePathRegularGameTwoPlayers = "src/test/resources/regular_game_two_players.txt";
+	final String filePathAllStrike = "src/test/resources/all_strikes.txt";
+	final String filePathAllFouls = "src/test/resources/all_fouls.txt";
 
-        FileRollRepository repository = new DefaultFileRollRepository();
+	FileRollRepository repository = new DefaultFileRollRepository();
 
-        List<String> regularGameOnePlayerLines = repository.load(filePathRegularGame);
-        List<String> allStrikesGameOnePlayerLines = repository.load(filePathAllStrike);
-        List<String> allFoulsOnePlayerLines = repository.load(filePathAllFouls);
-        List<String> regularGameTwoPlayerLines = repository.load(filePathRegularGameTwoPlayers);
+	List<String> regularGameOnePlayerLines = repository.load(filePathRegularGame);
+	List<String> allStrikesGameOnePlayerLines = repository.load(filePathAllStrike);
+	List<String> allFoulsOnePlayerLines = repository.load(filePathAllFouls);
+	List<String> regularGameTwoPlayerLines = repository.load(filePathRegularGameTwoPlayers);
 
-        RollParser parser = DefaultRollParserFactory.instance().get(RollParserType.DEFAULT);
+	RollParser parser = DefaultRollParserFactory.instance().get(RollParserType.DEFAULT);
 
-        List<PlayerFrames> regularGameOnePlayerParced = parser.parse(regularGameOnePlayerLines);
-        List<PlayerFrames> allStrikesGameOnePlayerParced = parser.parse(allStrikesGameOnePlayerLines);
-        List<PlayerFrames> allFoulsOnePlayerParced = parser.parse(allFoulsOnePlayerLines);
-        List<PlayerFrames> regularGameTwoPlayerParced = parser.parse(regularGameTwoPlayerLines);
+	List<PlayerFrames> regularGameOnePlayerParced = parser.parse(regularGameOnePlayerLines);
+	List<PlayerFrames> allStrikesGameOnePlayerParced = parser.parse(allStrikesGameOnePlayerLines);
+	List<PlayerFrames> allFoulsOnePlayerParced = parser.parse(allFoulsOnePlayerLines);
+	List<PlayerFrames> regularGameTwoPlayerParced = parser.parse(regularGameTwoPlayerLines);
 
-        FrameScoreProcessor processor = new DefaultFrameScoreProcessor();
+	FrameScoreProcessor processor = new DefaultFrameScoreProcessor();
 
-        this.regularGameOnePlayerProcessed = processor.processAll(regularGameOnePlayerParced);
-        this.allStrikesGameOnePlayerProcessed = processor.processAll(allStrikesGameOnePlayerParced);
-        this.allFoulsOnePlayerProcessed = processor.processAll(allFoulsOnePlayerParced);
-        this.regularGameTwoPlayerProcessed = processor.processAll(regularGameTwoPlayerParced);
-        this.formatter = new DefaultPlayerScoreFormatter();
+	this.regularGameOnePlayerProcessed = processor.processAll(regularGameOnePlayerParced);
+	this.allStrikesGameOnePlayerProcessed = processor.processAll(allStrikesGameOnePlayerParced);
+	this.allFoulsOnePlayerProcessed = processor.processAll(allFoulsOnePlayerParced);
+	this.regularGameTwoPlayerProcessed = processor.processAll(regularGameTwoPlayerParced);
+	this.formatter = new DefaultPlayerScoreFormatter();
     }
 
     @After
     public void tearDown() {
-        this.regularGameOnePlayerProcessed = null;
-        this.allStrikesGameOnePlayerProcessed = null;
-        this.allFoulsOnePlayerProcessed = null;
-        this.regularGameTwoPlayerProcessed = null;
-        this.formatter = null;
+	this.regularGameOnePlayerProcessed = null;
+	this.allStrikesGameOnePlayerProcessed = null;
+	this.allFoulsOnePlayerProcessed = null;
+	this.regularGameTwoPlayerProcessed = null;
+	this.formatter = null;
     }
 
     @Test(expected = NullPointerException.class)
     public void format_nullList_throwException() {
-        formatter.format(null);
+	formatter.format(null);
     }
 
     @Test
     public void format_validList_returnFormattedHeader() throws Exception {
-        String headerFormat = new String(Files.readAllBytes(Paths.get("src/test/resources/formatter/header.txt")));
-        GameScore gameScore = formatter.format(regularGameOnePlayerProcessed);
-        String header = gameScore.getHeader();
-        assertTrue(header.equals(headerFormat));
-    }
-    @Test
-    public void format_validList_returnFormattedScore() throws Exception {
-        String headerFormat = new String(Files.readAllBytes(Paths.get("src/test/resources/formatter/header.txt")));
-        GameScore gameScore = formatter.format(regularGameTwoPlayerProcessed);
-        String header = gameScore.getHeader();
-        assertTrue(header.equals(headerFormat));
+	String headerFormat = new String(Files.readAllBytes(Paths.get("src/test/resources/formatter/header.txt")));
+	GameScore gameScore = formatter.format(regularGameOnePlayerProcessed);
+	String header = gameScore.getHeader();
+	assertTrue(header.equals(headerFormat));
     }
 
+    @Test
+    public void format_validList_returnFormattedScore() throws Exception {
+	String scorePlayerOneFormat = new String(Files.readAllBytes(Paths.get("src/test/resources/formatter/player_one_score_regular.txt")));
+	String scorePlayerTwoFormat = new String(Files.readAllBytes(Paths.get("src/test/resources/formatter/player_two_score_regular.txt")));
+	GameScore gameScore = formatter.format(regularGameTwoPlayerProcessed);
+	List<String> players = gameScore.getPlayers();
+	String playerOneScore = players.get(0);
+	String playerTwoScore = players.get(1);
+	assertTrue(playerOneScore.equals(scorePlayerOneFormat));
+	assertTrue(playerTwoScore.equals(scorePlayerTwoFormat));
+    }
 }
